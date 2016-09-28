787f7d125cd87d000d9f6bfab2a8a02ce9e5c4ec
Hannah Marlin <hmmarlin@gmail.com>
2016-09-27 21:54:51 -0700

    add working app yay

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,29 +2,34 @@ var matches = [{
 	matchName: 'Yoda',
 	matchEmail: 'yoda@bikes.com',
 	matchPic: 'http://oi62.tinypic.com/314350y.jpg',
-	matchScores: [1, 2, 4, 3, 4, 4, 3, 2, 1, 5]
+	matchScores: [1, 2, 4, 3, 4, 4, 3, 2, 1, 5],
+	matchSum: 29
 }, {
 	matchName: 'Jax Teller',
 	matchEmail: 'jax@soa.com',
 	matchPic: 'https://d1bynyoa6h4b2m.cloudfront.net/wp-content/uploads/2015/07/jaxtribute-noplay.jpg',
-	matchScores: [4, 3, 4, 1, 2, 4, 5, 2, 1, 5]
+	matchScores: [4, 3, 4, 1, 2, 4, 5, 2, 1, 5],
+	matchSum: 31
 }, {
 	matchName: 'Evel Knievel',
 	matchEmail: 'evel@knievel.com',
 	matchPic: 'https://www.wired.com/images_blogs/photos/uncategorized/2007/11/30/evelk_4.jpg',
-	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 5, 5]
+	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 5, 5],
+	matchSum: 48
 }, {
 	matchName: 'Denim Dan',
 	matchEmail: 'danny@motorcycle.com',
 	matchPic: 'http://www.survivalplus.com/forward/images/biker_dude.jpg',
-	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 5, 5]
-}, {
-	matchName: 'evel knievel',
-	matchEmail: 'evel@knievel.com',
-	matchPic: 'https://www.wired.com/images_blogs/photos/uncategorized/2007/11/30/evelk_4.jpg',
-	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 5, 5]
-}];
+	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 1, 2],
+	matchSum: 41
 
+}, {
+	matchName: 'Vince Vaughn on a motorcycle',
+	matchEmail: 'vince@moto.com',
+	matchPic: 'https://s-media-cache-ak0.pinimg.com/236x/cb/27/31/cb273193d6fb32f861ac0c0198e0d911.jpg',
+	matchScores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+	matchSum: 30
+}];
 
 
 module.exports = matches;
