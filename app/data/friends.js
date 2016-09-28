e502a70c456f334ec3abd824fbf573b4227504dd
Hannah Marlin <hmmarlin@gmail.com>
2016-09-28 16:24:27 -0700

    add finalized app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,35 @@
 var matches = [{
 	matchName: 'Yoda',
-	matchEmail: 'yoda@bikes.com',
 	matchPic: 'http://oi62.tinypic.com/314350y.jpg',
 	matchScores: [1, 2, 4, 3, 4, 4, 3, 2, 1, 5],
 	matchSum: 29
 }, {
 	matchName: 'Jax Teller',
-	matchEmail: 'jax@soa.com',
 	matchPic: 'https://d1bynyoa6h4b2m.cloudfront.net/wp-content/uploads/2015/07/jaxtribute-noplay.jpg',
 	matchScores: [4, 3, 4, 1, 2, 4, 5, 2, 1, 5],
 	matchSum: 31
 }, {
 	matchName: 'Evel Knievel',
-	matchEmail: 'evel@knievel.com',
 	matchPic: 'https://www.wired.com/images_blogs/photos/uncategorized/2007/11/30/evelk_4.jpg',
 	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 5, 5],
 	matchSum: 48
 }, {
 	matchName: 'Denim Dan',
-	matchEmail: 'danny@motorcycle.com',
 	matchPic: 'http://www.survivalplus.com/forward/images/biker_dude.jpg',
 	matchScores: [5, 5, 5, 5, 4, 4, 5, 5, 1, 2],
 	matchSum: 41
 
 }, {
 	matchName: 'Vince Vaughn on a motorcycle',
-	matchEmail: 'vince@moto.com',
 	matchPic: 'https://s-media-cache-ak0.pinimg.com/236x/cb/27/31/cb273193d6fb32f861ac0c0198e0d911.jpg',
 	matchScores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
 	matchSum: 30
+},
+{
+matchName: "Charlie",
+matchPic: "http://mediad.publicbroadcasting.net/p/krcc/files/styles/medium/public/201603/mike-rust-c-1988-490x697.jpg",
+matchScores: [1, 2, 1, 1, 1, 1, 1, 1, 1, 5],
+matchSum: 15
 }];
 
 
