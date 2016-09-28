511a1768f4729b562c938d18958be346da0f17df
Hannah Marlin <hmmarlin@gmail.com>
2016-09-28 16:59:16 -0700

    get rid of dumb demo stuff, update port to 8080

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
-var cool = require('cool-ascii-faces');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 80;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -22,9 +22,6 @@ app.use(bodyParser.json({
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
 app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
